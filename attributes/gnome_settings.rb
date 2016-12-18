313377883c40c14da11741b97fc6217174bea9eb
sinkingpoint <colin@quirl.co.nz>
2016-12-19 12:50:56 +1300

    Added recipe to setup some gnome settings

---
+++ attributes/gnome_settings.rb
@@ -0,0 +1,15 @@
+default['workstation']['gnome']['keyboard_shortcuts'] = [
+  {
+    name: 'Terminal',
+    command: 'gnome-terminal',
+    binding: '<Primary><Alt><Meta>t'
+  }
+]
+
+default['workstation']['gnome']['extensions'] = [
+  {
+    name: 'Dash-To-Dock',
+    source: 'https://extensions.gnome.org/review/download/6265.shell-extension.zip',
+    destination: '/home/colin/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com'
+  }
+]
---
+++ recipes/init_gnome_settings.rb
@@ -0,0 +1,43 @@
+directory ::File.join(node['workstation']['user']['home'], '.firstrun') do
+  user node['workstation']['user']['name']
+  group node['workstation']['user']['group']
+  action :create
+end
+
+# Installs gnome extensions
+node['workstation']['gnome']['extensions'].each do |extension|
+  destination_file = ::File.join(Chef::Config[:file_cache_path], "#{extension[:name]}.zip")
+  remote_file destination_file do
+    source extension[:source]
+    user node['workstation']['user']['name']
+    group node['workstation']['user']['group']
+    notifies :run, "bash[Unzip gnome extension #{extension['name']}]"
+    action :create  
+  end
+
+  directory extension[:destination] do
+    recursive true
+    user node['workstation']['user']['name']
+    group node['workstation']['user']['group']
+    action :create
+  end
+
+  bash "Unzip gnome extension #{extension['name']}" do
+    code "unzip #{destination_file} -d #{extension[:destination]}"
+    user node['workstation']['user']['name']
+    group node['workstation']['user']['group']
+    action :nothing
+  end
+end
+
+# A few Gnome settings have to go in an sh file to be run on log in, 
+# because lots of things aren't setup until we log in
+template ::File.join(node['workstation']['user']['home'], '.firstrun', 'init_gnome_settings.sh') do
+  source 'gnome_settings.sh.erb'
+  variables ({
+    keybindings: node['workstation']['gnome']['keyboard_shortcuts']
+  })
+  user node['workstation']['user']['name']
+  group node['workstation']['user']['group']
+  action :create
+end
