88131e66a5ced8797eeaf1b34998239acbc84c56
sinkingpoint <colin@quirl.co.nz>
2017-04-26 00:42:52 +0100

    Added support for public github

--- recipes/init_github.rb
+++ recipes/init_github.rb
@@ -16,3 +16,17 @@ template ::File.join(node['workstation']['user']['home'], '.firstrun', 'init_git
   notifies :create, 'template[/etc/profile.d/firstrun.sh]'
   action :create
 end
+
+
+if node['workstation']['ssh']['enable_public_github']
+  template ::File.join(node['workstation']['user']['home'], '.firstrun', 'init_public_github.sh') do
+    source 'init_public_github.sh.erb'
+    variables ({
+      ssh_directory: ::File.join(node['workstation']['user']['home'], '.ssh')
+    })
+    user node['workstation']['user']['name']
+    group node['workstation']['user']['group']
+    notifies :create, 'template[/etc/profile.d/firstrun.sh]'
+    action :create
+  end
+end
